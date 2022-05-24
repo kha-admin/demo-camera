0c3e0a37d1939ed7b7a7e755dda1625d0e19a6b0
bankkasem <kasemsarn.purisarn@gmail.com>
2022-05-24 16:58:15 +0700

    feat: update `button-group` add label and set auto width

--- app/survey_website/components/button_group/index.tsx
+++ app/survey_website/components/button_group/index.tsx
@@ -8,27 +8,34 @@ export interface IOption {
 export interface IProps {
     value: string;
     options: Array<IOption>;
+    label?: string;
     setValue: React.Dispatch<string>;
 }
 
-const ButtonGroup: React.FC<IProps> = ({ value, options, setValue }) => {
+const ButtonGroup: React.FC<IProps> = ({ value, options, label, setValue }) => {
     const handleChange = (value: IOption['value']): void => {
         setValue(value);
     };
 
     return (
-        <div className="btn-group">
-            {options.map((item, index) => (
-                <input
-                    type={'radio'}
-                    name={'options'}
-                    key={index}
-                    checked={value === item.value}
-                    data-title={item.text}
-                    className="btn"
-                    onChange={() => handleChange(item.value)}
-                />
-            ))}
+        <div className="form-control w-full max-w-xs">
+            <label className="label">
+                <span className="label-text">{label}</span>
+            </label>
+            <div className="btn-group">
+                {options.map((item, index) => (
+                    <input
+                        type={'radio'}
+                        name={'options'}
+                        key={index}
+                        checked={value === item.value}
+                        data-title={item.text}
+                        className="btn"
+                        style={{ width: `${100 / options.length}%` }}
+                        onChange={() => handleChange(item.value)}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
