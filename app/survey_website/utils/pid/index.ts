d8dc72d8ed4c2b1ac9919ae4131962f6eb00731f
bankkasem <kasemsarn.purisarn@gmail.com>
2022-05-24 16:16:06 +0700

    feat: add `is-valid-thai-pid` function

---
+++ app/survey_website/utils/pid/index.ts
@@ -0,0 +1,23 @@
+export function isValidThaiPid(value: string): boolean {
+    const last = parseInt(value[value.length - 1], 10);
+    let sum = 0;
+    let count = 0;
+
+    for (let i = 13; i >= 2; i -= 1) {
+        sum += parseInt(value[count], 10) * i;
+        count += 1;
+    }
+
+    const step4 = sum % 11;
+    let step5 = 11 - step4;
+
+    if (step5 / 10 !== 0) {
+        step5 %= 10;
+    }
+
+    if (step5 === last && value.length === 13) {
+        return true;
+    }
+
+    return false;
+}
