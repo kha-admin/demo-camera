8bba548a16ee90f8e7a992e775aef24f04be88e1
worasitipon <worasitipon.t@hotmail.com>
2022-05-26 20:23:31 +0700

    feat(survey_website): add local client

--- app/survey_website/utils/client/api_client.ts
+++ app/survey_website/utils/client/api_client.ts
@@ -5,3 +5,9 @@ export type IKyInstance = typeof KY;
 export const thaikubClient = KY.extend({
     prefixUrl: 'https://thaiaddressapi-thaikub.herokuapp.com/',
 });
+
+export const localClient = KY.extend({
+    prefixUrl: process.env.NEXT_PUBLIC_BASE_URL,
+});
+
+export default KY;
