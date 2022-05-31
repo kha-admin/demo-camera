ebc1ce925b7bee5df6c1bebd5587d4af1dedaced
worasitipon <worasitipon.t@hotmail.com>
2022-05-31 18:35:34 +0700

    fix(survey_website): :bug: correct prefetching data

--- app/survey_website/utils/client/api_client.ts
+++ app/survey_website/utils/client/api_client.ts
@@ -2,10 +2,6 @@ import KY from 'ky';
 
 export type IKyInstance = typeof KY;
 
-export const thaikubClient = KY.extend({
-    prefixUrl: 'https://thaiaddressapi-thaikub.herokuapp.com/',
-});
-
 export const localClient = KY.extend({
     prefixUrl: process.env.NEXT_PUBLIC_BASE_URL,
 });
