1b34a296cbe736556f175e6f8f3721e1ecef28a1
bankkasem <kasemsarn.purisarn@gmail.com>
2022-06-01 11:19:32 +0700

    refactor: refactoring `input` remove wrapper

--- app/survey_website/components/input/index.tsx
+++ app/survey_website/components/input/index.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { type UseFormRegisterReturn } from 'react-hook-form';
 
-import InputWrapper from '../input_wrapper';
-
 export interface IProps {
-    value?: string;
     form?: Partial<UseFormRegisterReturn>;
-    type?: React.HTMLInputTypeAttribute;
-    label?: string;
+    type?: React.HTMLInputTypeAttribute; // input-type
     placeholder?: string;
-    message?: string;
     maxLength?: number;
     disabled?: boolean;
     ghost?: boolean; // no background
     success?: boolean; // success color
     error?: boolean; // error color
+    value?: string;
     onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 const Input: React.FC<IProps> = ({
     form,
     type = 'text',
-    label,
     placeholder,
-    message,
     maxLength,
     disabled,
     ghost,
@@ -32,22 +26,20 @@ const Input: React.FC<IProps> = ({
     ...props
 }) => {
     return (
-        <InputWrapper label={label} message={message}>
-            <input
-                {...form}
-                {...props}
-                type={type}
-                placeholder={placeholder}
-                maxLength={maxLength}
-                disabled={disabled}
-                className={[
-                    'input w-full',
-                    ghost ? 'input-ghost' : 'input-bordered',
-                    success ? 'input-success' : '',
-                    error ? 'input-error' : '',
-                ].join(' ')}
-            />
-        </InputWrapper>
+        <input
+            {...form}
+            {...props}
+            type={type}
+            placeholder={placeholder}
+            maxLength={maxLength}
+            disabled={disabled}
+            className={[
+                'input w-full',
+                ghost ? 'input-ghost' : 'input-bordered',
+                success ? 'input-success' : '',
+                error ? 'input-error' : '',
+            ].join(' ')}
+        />
     );
 };
 
