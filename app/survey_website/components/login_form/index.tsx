e09152e89d829ca458ec5ffd9cf53d3a7b1620aa
bankkasem <kasemsarn.purisarn@gmail.com>
2022-06-28 16:32:10 +0700

    fix: update `login` form

--- app/survey_website/components/login_form/index.tsx
+++ app/survey_website/components/login_form/index.tsx
@@ -1,19 +1,41 @@
+import Link from 'next/link';
 import React from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
 
 import Input from '@/components/input';
 import InputWrapper from '@/components/input_wrapper';
 
+export interface IForm {
+    username: string;
+    password: string;
+}
+
 const LoginForm: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IForm>();
+
+    const onSubmit: SubmitHandler<IForm> = (data) => {
+        console.log('onSubmit :', data);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <InputWrapper
                 label={
                     <p>
                         <span className="text-red-500">*</span> Login
                     </p>
                 }
+                error={!!errors.username}
+                message={errors.username?.message}
             >
-                <Input />
+                <Input
+                    form={register('username', { required: 'กรุณากรอกชื่อผู้ใช้งาน' })}
+                    error={!!errors.username}
+                />
             </InputWrapper>
 
             <InputWrapper
@@ -22,15 +44,24 @@ const LoginForm: React.FC = () => {
                         <span className="text-red-500">*</span> Password
                     </p>
                 }
+                error={!!errors.password}
+                message={errors.password?.message}
             >
-                <Input />
+                <Input
+                    form={register('password', { required: 'กรุณากรอกรหัสผ่าน' })}
+                    error={!!errors.password}
+                />
             </InputWrapper>
 
             <div className="w-full text-right">
-                <button className="btn btn-link">ลืมรหัสผ่าน?</button>
+                <Link href="/forgot-password">
+                    <button className="btn btn-link">ลืมรหัสผ่าน?</button>
+                </Link>
             </div>
 
-            <button className="btn btn-block btn-primary">เข้าสู่ระบบ</button>
+            <button type="submit" className="btn btn-block btn-primary">
+                เข้าสู่ระบบ
+            </button>
         </form>
     );
 };
