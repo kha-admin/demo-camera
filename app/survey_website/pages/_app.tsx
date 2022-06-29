d61821a5002c3f2dc0979bfd4e4313687fd59fc8
bankkasem <kasemsarn.purisarn@gmail.com>
2022-06-29 17:03:55 +0700

    feat: register `router-guard` and `refresh-access-token` plugin

--- app/survey_website/pages/_app.tsx
+++ app/survey_website/pages/_app.tsx
@@ -1,17 +1,26 @@
 import { AppProps } from 'next/app';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { QueryClientProvider, QueryClient, Hydrate } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import '@/styles/global.css';
+import RouterGuard from '@/components/router_guard';
+import { refreshAccessToken } from '@/plugins/refresh_access_token';
 
 const App: React.FC<AppProps<IPageProps>> = ({ Component, pageProps }) => {
     const [client] = useState(() => new QueryClient());
 
+    // register refresh access token plugin
+    useEffect(() => {
+        refreshAccessToken();
+    }, []);
+
     return (
         <QueryClientProvider client={client}>
-            <Hydrate state={pageProps.dehydratedState}>
-                <Component {...pageProps} />
+            <Hydrate state={pageProps?.dehydratedState}>
+                <RouterGuard>
+                    <Component {...pageProps} />
+                </RouterGuard>
                 <ReactQueryDevtools initialIsOpen={false} />
             </Hydrate>
         </QueryClientProvider>
