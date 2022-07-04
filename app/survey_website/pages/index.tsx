d9fa2b46ab69fcc28e0fca53cdfd9fc3891e086b
chatipum <chatipum@gmail.com>
2022-07-04 17:21:24 +0700

    test react-webcam

--- app/survey_website/pages/index.tsx
+++ app/survey_website/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type GetStaticProps } from 'next';
 import React from 'react';
 import { dehydrate, QueryClient } from 'react-query';
+import Webcam from 'react-webcam';
 
 import Layout from '@/components/layout';
 import { prefetchSanityCms } from '@/utils/prefetch';
@@ -13,6 +14,27 @@ const Home: React.FC = () => {
             <section className="relative w-full min-h-screen">
                 <div className="grid grid-cols-1 gap-6 lg:gap-10 p-6 lg:py-10">
                     <h1>Home Page</h1>
+                    <Webcam
+                        className="w-full rounded"
+                        audio={false}
+                        screenshotFormat="image/png"
+                        videoConstraints={{ width: 1920, height: 1080, facingMode: 'user' }}
+                    >
+                        {({ getScreenshot }) => (
+                            <button
+                                onClick={() => {
+                                    const imageSrc = getScreenshot({
+                                        width: 1920,
+                                        height: 1080,
+                                    });
+
+                                    console.log('imageSrc ===>', imageSrc);
+                                }}
+                            >
+                                capture
+                            </button>
+                        )}
+                    </Webcam>
                 </div>
             </section>
         </Layout>
